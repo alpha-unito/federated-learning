ccfe8c0dfe5c384f29177c183045b43f6f9ece37
lorenzo <lory.man95@gmail.com>
2019-10-17 15:46:07 +0200

    working mqtt version

--- Client/fl_runtime.py
+++ Client/fl_runtime.py
@@ -2,13 +2,15 @@ import requests
 import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, flags, rc):
-  print("Connected with result code "+str(rc))
-  client.subscribe("topic/fl")
+    print("Connected with result code "+str(rc))
+    client.subscribe("topic/fl-broadcast")
+
 
 def on_message(client, userdata, msg):
-  if msg.payload.decode() == "Hello world!":
-    print("Yes!")
-    client.disconnect()
+    print("received")
+    if msg.payload.decode() == "Hello world!":
+        print("Yes!")
+        client.disconnect()
 
 # MQTT CLIENT CONNECTION TO MESSAGE BROKER
 client = mqtt.Client()
@@ -17,11 +19,6 @@ client.connect("localhost", 1883, 60)
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.loop_forever()
-# client.disconnect()
-
-# api-endpoint 
-URL = "http://127.0.0.1:5002/selector"
-
 def device_connection_to_server():
-    client.publish("topic/fl", "Hello world!");
+    # client.publish("topic/fl-broadcast", "Hello world!");
+    client.loop_forever()
