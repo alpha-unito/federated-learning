984e6b2658e4cb85b48da4365ce8a943ecf214b0
lorenzo <lory.man95@gmail.com>
2020-02-24 18:18:48 +0100

    Update image taking and redefine imagenet classes

--- Client/ImageClassification/task.py
+++ Client/ImageClassification/task.py
@@ -2,43 +2,45 @@ from alex_net import AlexNet
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
 import numpy as np
+from imagenet_classes import classes as in_classes
+
+from os import listdir
+from os.path import isfile, join
 
 
-def load_images(path):
+def load_images(path: str, res = {}) -> dict:
     """
-    Read Image, and change to BGR
+    Read Image
 
-    im1 = (imread("./img/laska.png")[:,:,:3]).astype(float32)
-    im1 = im1 - mean(im1)
-    im1[:, :, 0], im1[:, :, 2] = im1[:, :, 2], im1[:, :, 0]
+    ::return:   {path: [image0, ..., imageN]} 
     """
 
-    l = []
     for f in listdir(path):
-        if isfile(join(path, f)):
-        print "f: ",f
-            l.append(join(path, f))
+        sub_path = join(path, f) 
+        
+        if isfile(sub_path):
+            res[path].append(get_image(sub_path))
         else:
-            l += listFile(join(path, f))
+            res[sub_path] = []
+            load_images(sub_path, res)
+    
+    return res
 
-    return l
 
-
-def pre_process(data):
+def get_image(path):
     """
-    Pre-process data for training
     """
-    array=[]
-    for im in data:
-        a = (imread(im)[:,:,:3]).astype(float32)
-        a = a - mean(a)
-        a[:, :, 0], a[:, :, 2] = a[:, :, 2], a[:, :, 0]
-        array.append(a)
+    
+    img = (imread(im)[:,:,:3]).astype(float32)
+    
+    # pre-processing
+    img = img - mean(img)
+    # a[:, :, 0], a[:, :, 2] = a[:, :, 2], a[:, :, 0] # Pre-process data changing to RGB to BGR (maybe useless)
+        
+    return img
 
-    return array
 
-
-def training(model, data):
+def training(model, x_train, y_train):
     """
     Training
     """
@@ -55,18 +57,18 @@ def training(model, data):
     print('\nhistory dict:', history.history)
 
 
-def evaluate(model, data):
+def evaluate(model, x_test, y_test):
     # Evaluate the model on the test data using `evaluate`
     print('\n# Evaluate on test data')
     results = model.evaluate(x_test, y_test, batch_size=128)
     print('test loss, test acc:', results)
 
 
-def predict(model, data):
+def predict(model, x_val):
     # Generate predictions (probabilities -- the output of the last layer)
     # on new data using `predict`
     print('\n# Generate predictions for 3 samples')
-    predictions = model.predict(x_test[:3])
+    predictions = model.predict(x_val[:3])
     print('predictions shape:', predictions.shape)
 
 
@@ -74,12 +76,21 @@ def if __name__ == "__main__":
     
     model = AlexNet()
 
-    data = load_images('path')
+    training_set = load_images('./res/train') # {path: [image0, ..., imageN]} 
 
-    training_set = pre_process(data)
 
-    train(model, training_set)
+    # manipulate training set and divide images from labels
+    x_train = []
+    y_train = []
+
+    for label, images in training_set.items():
+        for image in images:
+            x_train.append(image)
+            y_train.append(label)
+
+
+    train(model, x_train, y_train)
     
-    evaluate(model, training_set)
+    evaluate(model, x_train, y_train)
     
-    predict(model, training_set)
+    # predict(model, training_set)
