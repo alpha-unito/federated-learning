8d8faee4d0aaf8b00dc74d6b3abb374bffe682ea
lorenzo <lory.man95@gmail.com>
2020-03-08 18:01:16 +0100

    Update fl runtime for image classification

--- Client/app.py
+++ Client/app.py
@@ -1,5 +1,19 @@
-from fl_runtime import *
+import fl_runtime
 import time
 
+client_id = int(time.time())
+
 if __name__ == "__main__":
-    device_connection_to_server()
+    while True:
+        print(f"[CLIENT {client_id}]")
+        print(" - Insert 1 for start learning task")
+        print(" - Insert 2 for communicate the result to the FL server")
+
+        choice = input()
+
+        if choice == '1':
+            fl_runtime.training()
+        elif choice == '2':
+            fl_runtime.device_connection_to_server()
+        else:
+            print("Invalid option.\n")
--- Client/fl_runtime.py
+++ Client/fl_runtime.py
@@ -1,71 +1,30 @@
+import datetime
+import json
 import paho.mqtt.client as mqtt
 import tensorflow as tf
-import tensorflow_federated as tff
-import datetime
-import warnings
-import numpy as np
-import six
-import collections
-import json
+import keras
+from keras.preprocessing.image import ImageDataGenerator
 
-NUM_EPOCHS = 10
+IMAGENET_PATH = '/media/lore/6B6223601B584A05/IMAGENET/'
+TOTAL_IMAGES = 100
+TARGET_SIZE = (224, 224)
 BATCH_SIZE = 20
-SHUFFLE_BUFFER = 500
+EPOCHS = 3
+
+model = keras.applications.mobilenet_v2.MobileNetV2()
 
 
-
-warnings.simplefilter('ignore')
-tf.compat.v1.enable_v2_behavior()
-np.random.seed(0)
-
-if six.PY3:
-  tff.framework.set_default_executor(tff.framework.create_local_executor())
-
-
-
-def preprocess(dataset):
-    """
-    helper preprocess for building federated data
-    """
-    def element_fn(element):
-        return collections.OrderedDict([
-            ('x', tf.reshape(element['pixels'], [-1])),
-            ('y', tf.reshape(element['label'], [1])),
-        ])
-
-    return dataset.repeat(NUM_EPOCHS).map(element_fn).shuffle(
-        SHUFFLE_BUFFER).batch(BATCH_SIZE)
-
-
-"""
-NOTE: The data sets returned by load_data() are instances of tff.simulation.ClientData, an interface that allows 
-you to enumerate the set of users, to construct a tf.data.Dataset that represents the data of a particular 
-user, and to query the structure of individual elements. Here's how you can use this interface to explore 
-the content of the data set. Keep in mind that while this interface allows you to iterate over clients ids, 
-this is only a feature of the simulation data. As you will see shortly, client identities are not used by 
-the federated learning framework - their only purpose is to allow you to select subsets of the data for simulations.
-"""
-
-print('\nImporting test dataset...')
-emnist_train, emnist_test = tff.simulation.datasets.emnist.load_data()
-
-print('Test dataset imported.\n')
-
-
+# -------- MQTT UTILS -----------------------
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print(f"Connected with result code {rc}")
     client.subscribe("topic/fl-broadcast")
-
-
 
 def on_message(client, userdata, msg):
     print("received")
     if msg.payload.decode() == "Hello world!":
         print("Yes!")
         client.disconnect()
-
-
 
 # MQTT CLIENT CONNECTION TO MESSAGE BROKER
 client = mqtt.Client()
@@ -74,19 +33,55 @@ client.connect("localhost", 1883, 60)
 client.on_connect = on_connect
 client.on_message = on_message
 
+# -------- END MQTT UTILS ------------------
+
+def calculate_steps():
+    steps = 1
+    parts = TOTAL_IMAGES / BATCH_SIZE
+    if parts > 1:
+        steps = parts
+        if TOTAL_IMAGES % BATCH_SIZE > 0:
+            steps += 1
+
+    return steps
+
+def training():
+    global model
+    model.summary()
+
+    # Compile the model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    # create generator
+    datagen = ImageDataGenerator()
+
+    # prepare an iterators for each dataset
+    train_path = "../res/train"
+    train_it = datagen.flow_from_directory(train_path,
+                                           target_size=TARGET_SIZE,
+                                           class_mode='categorical',
+                                           color_mode='rgb',
+                                           batch_size=BATCH_SIZE)
+
+    model.fit_generator(train_it, steps_per_epoch=calculate_steps(), epochs=EPOCHS)
+
+    return model
 
 
 def device_connection_to_server():
-    sample_client = emnist_train.client_ids[0] 
 
     # select training data related to selected clients
-    federated_train_data = preprocess(emnist_train.create_tf_dataset_for_client(sample_client))
-    print(federated_train_data)
+
+    # build message object
+    send_msg = {}
+    """
     send_msg = {
         'device': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         'data': tf.nest.map_structure(lambda x: x.numpy().tolist(), iter(federated_train_data).next())
     }
+    """
 
+    # publishes on MQTT topic
     client.publish("topic/fl-broadcast", json.dumps(send_msg));
     
     print("\npublished message to 'topic/fl-broadcast'")
